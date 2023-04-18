88ea04aa2666e03e95bd9e99aa269baf1fd1de4e
consuti-tec <consuti@consuti.com.br>
2023-04-18 00:47:29 -0300

    first commit

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  ColorTheme: "#FC6405",
-  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/estoril-esportes",
+  ColorTheme: "#D61F27",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/bach-auer-assessoria-e-servicos-esportivos-ltda",
   UrlPainel: "https://painel.escoladoesporte.com.br/",
   TimeSplashScreen: 3000,
 };
